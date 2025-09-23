ce76939c3cee36ad846d417780d194d70a6752be
Knarta <charlene.martin@holbertonstudents.com>
2025-09-23 14:14:30 +0200

    Add welcome function in 0-welcome.js to greet users with their full name, enhancing user interaction and personalization.

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,9 @@
+// Welcome function
+
+function welcome(firstName, lastName) {
+    let fullName =  firstName + " " + lastName;
+    function displayFullName() {
+        alert("Welcome" + fullName + "!");
+    }
+    displayFullName();
+}
